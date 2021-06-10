2cee4cd1c4f3258f823ade2503239b169782b7fa
JMC <jmch0616@gmail.com>
2021-06-10 22:38:18 +0900

    basics UI

--- app/src/main/java/com/example/day_19/MainActivity.kt
+++ app/src/main/java/com/example/day_19/MainActivity.kt
@@ -4,6 +4,8 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,20 +13,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var nowdate = LocalDate.now()
+        var Str_nowdate = nowdate.format(DateTimeFormatter.ofPattern("yyyy-mm-dd"))
+
+
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
 
         date_button.setOnClickListener {
-            val date_option = DatePickerDialog(this,
+            val start_date = DatePickerDialog(this,
                 DatePickerDialog.OnDateSetListener { view, mYear, mMonth, mDay ->
 
-                    date_text.setText("" + mDay + "/" + mMonth + "/" + mYear)
+                    start_date_detail.setText("" + mYear + "년 " + mMonth + "월 " + mDay + "일 ")
                 }, year, month, day)
 
-            date_option.show()
+            start_date.show()
         }
-    }
+        // var end_date = c.add(Calendar.DATE,14)
+        end_date_detail.setText("${Str_nowdate}")
 
+    }
 }
