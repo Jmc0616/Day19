359704e1f51744bb032bb8b733686e7bc37b62bd
JMC <jmch0616@gmail.com>
2021-06-15 20:07:41 +0900

    update date

--- app/src/main/java/com/example/day_19/MainActivity.kt
+++ app/src/main/java/com/example/day_19/MainActivity.kt
@@ -4,7 +4,10 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.Period
 import java.time.format.DateTimeFormatter
 import java.util.*
 
@@ -13,26 +16,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var nowdate = LocalDate.now()
-        var Str_nowdate = nowdate.format(DateTimeFormatter.ofPattern("yyyy-mm-dd"))
+        // 현재 날짜
+        val now = LocalDate.now()
+        val formatDate = DateTimeFormatter.ISO_DATE
+        val nowDate = now.format(formatDate)
+        now_date_detail.setText(""+ now)
 
-
+        // 격리 시작일
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
 
         date_button.setOnClickListener {
-            val start_date = DatePickerDialog(this,
+            var end_day = DatePickerDialog(this,
+                DatePickerDialog.OnDateSetListener { view, mYear, mMonth, mDay ->
+
+                    end_date_detail.setText("" + mYear + "년 " + mMonth + "월 " + mDay + "일 ")
+                }, year, month, day)
+
+            end_day.show()
+
+            var start_day = DatePickerDialog(this,
                 DatePickerDialog.OnDateSetListener { view, mYear, mMonth, mDay ->
 
                     start_date_detail.setText("" + mYear + "년 " + mMonth + "월 " + mDay + "일 ")
                 }, year, month, day)
 
-            start_date.show()
+            start_day.show()
         }
-        // var end_date = c.add(Calendar.DATE,14)
-        end_date_detail.setText("${Str_nowdate}")
+        // var time_count = nowDate - end_date
+        // var EndDate = LocalDate.of(start_day)
+
+        // var D_day = Period.between(now, EndDate)
+
+        // start_date.setText(""+D_day.getDays())
 
     }
 }
